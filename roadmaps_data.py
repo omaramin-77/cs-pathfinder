4d3412ec7d813c7b5ce69a38c8f8ad39d397ff2e
Marwan-Ghazal <marwanghazal002y@gmail.com>
2025-11-30 12:08:10 +0100

    add roadmap data module with ROADMAPS structure (#1)

---
+++ roadmaps_data.py
@@ -0,0 +1,14 @@
+"""
+Roadmap data for all career fields.
+
+Each roadmap is a list of steps with:
+- text (short step title)
+- description (what to do / learn)
+- course (URL to a resource)
+"""
+
+ROADMAPS = {
+    # Filled per career:
+    # "AI Engineer": [...],
+    # "ML Engineer": [...],
+}
