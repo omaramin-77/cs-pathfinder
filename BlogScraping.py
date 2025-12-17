f54fb565518d725afa5b6cf3df5cdaef6865516f
Marwan-Ghazal <marwanghazal002y@gmail.com>
2025-12-17 19:53:01 +0100

    testing code

--- BlogScraping.py
+++ BlogScraping.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import feedparser
 import requests
 from datetime import datetime
-from DB import get_db_connection
+
 from bs4 import BeautifulSoup
 
 RSS_FEED_URL = "https://www.kdnuggets.com/feed"
